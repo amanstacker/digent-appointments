47188910827fa469d7bda8fff4c3177e3804695b
amanstacker <amanstacker@gmail.com>
2026-05-23 18:53:42 +0530

    .
    .

--- includes/admin/settings/notifications/general.php
+++ includes/admin/settings/notifications/general.php
@@ -14,31 +14,33 @@ function dgap_register_general_notifications_section() {
 
     add_settings_field(
 			'enable_notifications',
-			__( 'Enable Notifications', 'digent-appointments' ),
+			esc_html__( 'Enable Notifications', 'digent-appointments' ),
 			[ 'DGAP_Settings_Fields', 'toggle' ],
 			'dgap_notifications_general',
 			'dgap_notifications_general',
 			[ 'id' => 'enable_notifications' ]
 		);
 
-		add_settings_field(
-			'enable_notification_logs',
-			__( 'Enable Logs', 'digent-appointments' ),
-			[ 'DGAP_Settings_Fields', 'toggle' ],
-			'dgap_notifications_general',
-			'dgap_notifications_general',
-			[ 'id' => 'enable_notification_logs' ]
-		);
+	add_settings_field(
+		'enable_notification_logs',
+		esc_html__( 'Enable Logs', 'digent-appointments' ),
+		[ 'DGAP_Settings_Fields', 'toggle' ],
+		'dgap_notifications_general',
+		'dgap_notifications_general',
+		[ 'id' => 'enable_notification_logs' ]
+	);
 
-		add_settings_field(
-			'notification_retry',
-			__( 'Retry Failed Notifications', 'digent-appointments' ),
-			[ 'DGAP_Settings_Fields', 'number' ],
-			'dgap_notifications_general',
-			'dgap_notifications_general',
-			[ 'id' => 'notification_retry', 'min' => 0, 'step' => 1 ]
-		);
+	add_settings_field(
+		'notification_retry',
+		esc_html__( 'Retry Failed Notifications', 'digent-appointments' ),
+		[ 'DGAP_Settings_Fields', 'number' ],
+		'dgap_notifications_general',
+		'dgap_notifications_general',
+		[ 'id' => 'notification_retry', 'min' => 0, 'step' => 1 ]
+	);
+
 }
+
 add_action( 'admin_init', 'dgap_register_general_notifications_section' );
 
 
